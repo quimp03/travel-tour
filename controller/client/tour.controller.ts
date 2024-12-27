2b09caeece5045bb844cd2d0d6ad679871ac6645
Mai Phúc Quí <maiphucqui8482@gmail.com>
2024-12-28 00:39:13 +0700

    get tours by category

--- controller/client/tour.controller.ts
+++ controller/client/tour.controller.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from "express"
 import Tour from "../../model/tour.modle";
+import sequelize from "../../config/database";
+import {QueryTypes } from "sequelize";
 export const index = async (req: Request, res: Response) => {
-    const tours = await Tour.findAll({
-        where: {
-            deleted: false,
-            status: "active"
-        },
-        raw: true
-    })
+    const slug = req.params.slugCategory
+    const tours = await sequelize.query(
+         `select tours.*, ROUND(price * ( 1 - discount/100),0)  as special_price from tours 
+         join tours_categories on tours.id = tours_categories.tour_id join categories
+          on tours_categories.category_id = categories.id
+          where categories.slug = '${slug}'
+          and tours.deleted = false
+          and categories.deleted = false
+          `,
+          {
+            type: QueryTypes.SELECT
+          }
+    )
+    tours.forEach(tour => {
+        if(tour["images"]){
+            const images = JSON.parse(tour["images"]); //change from json to str
+            tour["img"]  = images[0] //get first img
+        }
+        tour["special_price"] = parseFloat(tour["special_price"]) 
+    });
     res.render("client/pages/tours/index.pug", {
         tours: tours
     })
--- routes/client/tours.router.ts
+++ routes/client/tours.router.ts
@@ -2,5 +2,5 @@ import express from "express"
 import { Router } from "express";
 import * as controller from "../../controller/client/tour.controller"
 const router = express()
-router.get("/", controller.index)
+router.get("/:slugCategory",  controller.index)
 export const tour: Router = router;
