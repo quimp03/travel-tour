acd838434bd18e099f1d482c8644392c5cdc1033
Mai Phúc Quí <maiphucqui8482@gmail.com>
2025-01-04 02:04:44 +0700

    add tour to card

--- public/js/script.js
+++ public/js/script.js
@@ -15,3 +15,31 @@
       swiper: swiper,
     },
   });
+//cart
+const cartLocalStorage = localStorage.getItem("cart")
+if(!cartLocalStorage){
+  localStorage.setItem("cart", JSON.stringify([]))
+}
+const cartForm = document.querySelector("[form-add-to-cart]")
+if(cartForm){
+  cartForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const tourId =parseInt(cartForm.getAttribute("tour-id")) 
+    const quantity = parseInt(event.target.elements.quantity.value)
+    if(quantity > 0 && tourId){
+      const cart = JSON.parse(localStorage.getItem("cart"))
+      const isExitTour = cart.findIndex(item => item.tourId == tourId)
+      if(isExitTour == -1){
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        })
+      }else{
+        cart[isExitTour].quantity = cart[isExitTour].quantity + quantity
+      }
+      console.log(cart)
+      localStorage.setItem("cart", JSON.stringify(cart))
+    }
+  })
+}
+//end cart
